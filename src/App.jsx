a08e649b86ae67bc37fcd87faa8a91b854eeac48
otu <otu5795123@gmail.com>
2026-09-12 14:03:13 +0200

    Initial commit: React PWA template

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <header>
+      <h1>Manager wydatków</h1>
+      <p>Szablon.</p>
+    </header>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'Money Manager',
+        short_name: 'MM',
+        description: 'Simple money manager',
+        theme_color: '#111111',
+        background_color: '#f6f6f6',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+})
